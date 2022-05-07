6f29926ca813db4b77be67e487342864a857c04d
Alex Vazquez <avazque008@outlook.com>
2022-05-07 18:07:36 -0400

    created classes and rearranged the codebase

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const { promptManager, promptTeam } = require('./lib/prompt');
+const promptManager = require('./lib/prompt/promptManager');
+const promptEngineer = require('./lib/prompt/promptEngineer');
+const promptIntern = require('./lib/prompt/promptIntern');
+const Manager = require('./lib/Manager');
 
-promptManager()
-    .then(portfolioData => {
-        return promptTeam(portfolioData);  
-    })
-    .then(teamData => {
-        console.log(teamData);
-    });
+const startApp = () => {
+    const employeeInfo = [];
+    const managerInfo = [];
+
+    promptManager()
+        .then(info => {
+            const manager = new Manager(info.managerName, info.managerID, info.managerEmail, info.managerNumber);
+            managerInfo.push(manager);
+        });
+};
+
+startApp();
