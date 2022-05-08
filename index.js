9063f595319a62eb4c9d2fd41464cd3240bcd7a6
Alex Vazquez <avazque008@outlook.com>
2022-05-07 21:34:29 -0400

    used prompt input to create classes

--- index.js
+++ index.js
@@ -2,16 +2,61 @@ const promptManager = require('./lib/prompt/promptManager');
 const promptEngineer = require('./lib/prompt/promptEngineer');
 const promptIntern = require('./lib/prompt/promptIntern');
 const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+
+let employeesInfo = [];
 
 const startApp = () => {
-    const employeeInfo = [];
-    const managerInfo = [];
+    let managerInfo = [];
 
     promptManager()
         .then(info => {
             const manager = new Manager(info.managerName, info.managerID, info.managerEmail, info.managerNumber);
+            
             managerInfo.push(manager);
+            console.log(managerInfo);
+
+            confirmTeam();
         });
 };
 
+const confirmTeam = () => {
+
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'Please select one of the choices provided below:',
+            choices: ['Add an Engineer', 'Add an Intern', new inquirer.Separator(), 'Exit / Generate Team Profile']        
+        }
+    ])
+    .then(choice => {
+        switch (choice.addMember) {
+            case 'Add an Engineer':
+                promptEngineer()
+                    .then(info => {
+                        const engineer = new Engineer(info.engineerName, info.engineerID, info.engineerEmail,  info.engineerGitHub);
+                        employeesInfo.push(engineer);
+                        confirmTeam();
+                    });
+                break;
+            case 'Add an Intern':
+                promptIntern()
+                    .then(info => {
+                        const intern = new Intern(info.internName, info.internID, info.internEmail,  info.internSchool);
+                        employeesInfo.push(intern);
+                        confirmTeam();
+                    });
+                break;
+            case 'Exit / Generate Team Profile':
+                console.log(employeesInfo);
+                break;           
+        }
+    });
+};
+
+
 startApp();
+
